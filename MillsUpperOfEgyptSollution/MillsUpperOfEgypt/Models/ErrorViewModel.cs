e93bc54a3ecfa5f42554ad65d103928348625309
AliZakariaAlFateh <58048016+AliZakariaAlFateh@users.noreply.github.com>
2023-12-17 06:47:43 +0200

    First Commit

---
+++ MillsUpperOfEgyptSollution/MillsUpperOfEgypt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MillsUpperOfEgypt.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
